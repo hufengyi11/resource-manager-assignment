d8e7176681dae86899c8d8c7ef74ad93b84c24f3
Fengyi <hufengyi1997@icloud.com>
2022-08-09 17:02:39 +0100

    implement create assignment

--- impl/impl.go
+++ impl/impl.go
@@ -2,11 +2,21 @@ package impl
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
 
 	gen "assignment/gen/go"
 
+	"google.golang.org/grpc"
+	gRPC "google.golang.org/grpc"
+
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 )
 
@@ -20,18 +30,72 @@ type AssignmentItem struct {
 	PeopleID  string             `bson:"people_id"`
 }
 
-func (U *AssignmentServiceServerImpl) CreateAssignment(context.Context, *gen.CreateAssignmentReq) (*gen.CreateAssignmentRes, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateAssignment not implemented")
+func mongoNewClient() (*mongo.Client, *mongo.Collection, error) {
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	resourceManagerDB := client.Database("Assignment")
+	assignmentCollection := resourceManagerDB.Collection("assignment")
+
+	return client, assignmentCollection, nil
 }
+
+func projectServiceConnection() *grpc.ClientConn {
+	serverAddress := "rm-p-ms-t-srv:8080"
+	// serverAddress := "host.docker.internal:8080"
+
+	conn, e := gRPC.Dial(serverAddress, gRPC.WithTransportCredentials(insecure.NewCredentials()))
+	if e != nil {
+		fmt.Printf("grpc connection failed: %v", e)
+	}
+	defer conn.Close()
+
+	return conn
+}
+
+func (U *AssignmentServiceServerImpl) CreateAssignment(ctx context.Context, req *gen.CreateAssignmentReq) (*gen.CreateAssignmentRes, error) {
+	client, collection, err := mongoNewClient()
+	if err != nil {
+		log.Fatalf("mongoNewClient error %v \n", err)
+	}
+	conn := projectServiceConnection()
+	client := gen.NewAssignmentServiceClient(conn)
+	assignment := req.GetAssignment()
+	// verify if the project exists, and person exists
+
+	res, error := collection.InsertOne(
+		ctx, bson.D{
+			{Key: "Id", Value: assignment.Id},
+			{Key: "ProjectId", Value: assignment.ProjectId},
+			{Key: "PeopleId", Value: assignment.PeopleId},
+		})
+
+	if error != nil {
+		log.Fatal(error)
+	}
+	fmt.Println(res.InsertedID)
+
+	return &gen.CreateAssignmentRes{Assignment: assignment}, nil
+}
+
 func (U *AssignmentServiceServerImpl) ReadByPeopleAssignment(context.Context, *gen.ReadAssignmentByPeopleReq) (*gen.ReadAssignmentByPeopleRes, error) {
+
 	return nil, status.Errorf(codes.Unimplemented, "method ReadByPeopleAssignment not implemented")
 }
+
 func (U *AssignmentServiceServerImpl) ReadByProjectAssignment(context.Context, *gen.ReadAssignmentByProjectReq) (*gen.ReadAssignmentByProjectRes, error) {
+
 	return nil, status.Errorf(codes.Unimplemented, "method ReadByProjectAssignment not implemented")
 }
+
 func (U *AssignmentServiceServerImpl) UpdateAssignment(context.Context, *gen.UpdateAssignmentReq) (*gen.UpdateAssignmentRes, error) {
+
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateAssignment not implemented")
 }
+
 func (U *AssignmentServiceServerImpl) DeleteAssignment(context.Context, *gen.DeleteAssignmentReq) (*gen.DeleteAssignmentRes, error) {
+
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteAssignment not implemented")
 }
--- main.go
+++ main.go
@@ -4,16 +4,39 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
+
+	gen "assignment/gen/go"
+	impl "assignment/impl"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"google.golang.org/grpc"
 )
+
+var db *mongo.Client
+var projectdb *mongo.Collection
+var monogoCtx context.Context
 
 // export MONGODB_URI='mongodb+srv://dbUser:dbUserPassword@cluster0.kjucuqb.mongodb.net/?retryWrites=true&w=majority'
 func main() {
+
+	opts := []grpc.ServerOption{}
+	var grpcServer = grpc.NewServer(opts...)
+	var assignmentServer = impl.AssignmentServiceServerImpl{}
+	gen.RegisterAssignmentServiceServer(grpcServer, &assignmentServer)
+
+	address := ":8080"
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("could not listen to %s %v !!!!!!", address, err)
+	}
+
+	fmt.Println("successfully connect to grpc at port:", address)
+
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
@@ -30,7 +53,7 @@ func main() {
 		fmt.Printf("Ping Error: %v \n", err)
 	}
 
-	assignmentCollection := client.Database("Assignment").Collection("assignment")
+	// assignmentCollection := client.Database("Assignment").Collection("assignment")
 
 	// assignment := bson.D{{"id", 1}, {"project", 1}, {"people", 1}}
 	// result, err := assignmentCollection.InsertOne(context.Background(), assignment)
@@ -40,5 +63,6 @@ func main() {
 	// }
 
 	// fmt.Println(result.InsertedID)
+	log.Fatalln(grpcServer.Serve(lis))
 
 }
